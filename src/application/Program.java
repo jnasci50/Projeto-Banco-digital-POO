d9ab67c1ef087a2641ffa69b720ca7ff369163bc
Jnasci50 <jairoarea1@hotmail.com>
2021-12-24 23:03:02 -0300

    Conclusão do programa. Iniciando declaração de comentarios

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,39 +14,55 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
+        /**Declaração dos Objetos apartir das classes Account e UsserValidation*/
+
         Account account;
         UserValidation usuario;
+
+        /**Declaração de Variaveis Globais para confirmação de usuario e senha*/
 
         String confirmarUsario = " ";
         String confirmarSenha = " ";
 
-        System.out.print("Bem vindo ao JavaBank. ");
+        /**Inicio da interação com o correntista*/
+
+        System.out.print("Bem vindo ao JavaBank. O seu Banco Digital.");
         System.out.println();
 
-        System.out.println("Cadastre Seu usuario");
+        System.out.println("Por Favor Cadastre Seu Usuario.");
         String cadastrarUsuario = sc.nextLine();
 
-        System.out.println("Cadastre Seu Senha");
+        System.out.println("Por Favor Cadastre Seu Senha.");
         String cadastrarSenha = sc.nextLine();
+
+        /**Criação do objeto usuario*/
 
         usuario = new UserValidation(cadastrarUsuario,cadastrarSenha);
 
+        /**Camada do método para cadastro de usuario*/
+
         usuario.CadastrarUsario(usuario.getCadastroUsuario());
+
+        /**Camada do método para cadastro de usuario*/
+
+        usuario.CadastrarSenha(usuario.getCadastroSenha());
 
         System.out.println("Confirme seu usuario");
 
         confirmarUsario = sc.nextLine();
 
-        usuario.CadastrarSenha(usuario.getCadastroSenha());
-
         System.out.println("Confirme sua Senha");
 
         confirmarSenha = sc.nextLine();
+
+        /**Camada do método para confirmar de validação do usuario, após cadastramento.*/
 
         if (usuario.getUsuarioCadastrado()){
 
             usuario.ValidarUsuario(usuario.getValidarUsuario(), confirmarUsario);
         }
+
+        /**Camada do método para confirmar de validação de senha, após cadastramento.*/
 
         if (usuario.getSenhaCadastrada()){
 
@@ -54,8 +70,7 @@ public class Program {
         }
 
 
-        // apos cadastro e confirmação do usuario
-
+        /** Apos cadastro e confirmação do dados. Exibição dos dados da Conta. */
 
 
         int number = Account.numeroConta();
@@ -65,10 +80,21 @@ public class Program {
 
         System.out.println("Titular da Conta: " + usuario.getValidarUsuario());
 
+
         String holder = usuario.getValidarUsuario();
 
 
-        if (usuario.getUsuarioValidado() && usuario.getSenhaValidada()){
+        /** 2º Oportunidade de Confirmações caso aconteca digitação errada de dados pelo usuario*/
+
+        if (!usuario.getUsuarioValidado() && !usuario.getSenhaValidada()){
+
+            System.out.println("Por favor Valide Seus dados pela 2° vez");
+            usuario.ValidarUsuario(usuario.getValidarUsuario(), confirmarUsario);
+            usuario.ValidacaoSenha(usuario.getValidarSenha(),confirmarSenha);
+        }
+
+        /** Dados Corretos Chamada da classe Account*/
+        else {
 
             System.out.print("Existe um deposito inicial? (y/n)? ");
 
@@ -77,14 +103,14 @@ public class Program {
             if (response == 'y') {
                 System.out.print("Insira o valor do deposito inicial: ");
                 double initialDeposit = sc.nextDouble();
-                account = new Account(number, holder, initialDeposit);
+                account = new Account(number, holder, initialDeposit); /*Construtos com 3 atributos */
             } else {
-                account = new Account(number, holder);
+                account = new Account(number, holder); /*Construtos com 2 atributos */
             }
 
             System.out.println();
             System.out.println("Dados da sua conta:");
-            System.out.println(account);
+            System.out.println(account); /*Exibição dos dados da conta atualizados */
 
             System.out.println();
 
@@ -92,23 +118,13 @@ public class Program {
 
             int select = sc.nextInt();
 
-            account.selectedOperation(select);
+            account.selectedOperation(select); /*Seleção de operações pelo usuario */
             System.out.println(account);
-            System.out.println(account.mensagemFinal());
+            System.out.println(account.mensagemFinal()); /*messagem final do programa */
 
-            sc.close();
-
-        }else {
-            System.out.println("Por favor Valide Seus dados pela 2° vez");
-            usuario.ValidarUsuario(usuario.getValidarUsuario(), confirmarUsario);
-            usuario.ValidacaoSenha(usuario.getValidarSenha(),confirmarSenha);
         }
-/**
- * inicio da Intaração entre class.
- *
- * */
 
-
+        sc.close();
 
     }
 
